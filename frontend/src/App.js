eba52f4fcd7cc3c0c43280986d8a13f1074704b4
musstaffali <mamustafa@gtcc.edu>
2021-08-22 19:28:36 -0400

    frat

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import Profile from './components/Profile/Profile';
+import Books from './components/Books/Books';
+import AddBook from './components/Books/AddBook';
+import UpdateProfile from './components/UpdateProfile/UpdateProfile';
+import BookDetail from './components/Books/BookDetail';
+import Users from './components/Users/Users';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/register' component={Register} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/profile' component={Profile} />
+        <Route exact path='/books' component={Books} />
+        <Route exact path='/addbook' component={AddBook} />
+        <Route exact path='/user-update' component={UpdateProfile} />
+        <Route exact path='/book/:id' component={BookDetail} />
+        <Route exact path='/users' component={Users} />
+      </BrowserRouter>
+    </>
   );
-}
+};
 
 export default App;
